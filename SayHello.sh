c1545cf3559e1ef7fcd3727663e635ebb802e115
Evan Allen <eallen@xpirit.com>
2023-09-20 12:54:05 -0400

    add build

--- SayHello.sh
+++ SayHello.sh
@@ -5,7 +5,7 @@ repo_name="ActionContainer"
 response=$(gh issue list -R="$repo_owner/$repo_name" -s="open" -l=release)
 
 if echo "$response" | grep -q "no issues match your search"; then
-    echo "IS_RELEASE=false" >> "$GITHUB_OUTPUT"
+    echo "IS_RELEASE=false"
 else
-    echo "IS_RELEASE=true" >> "$GITHUB_OUTPUT"
+    echo "IS_RELEASE=true"
 fi
