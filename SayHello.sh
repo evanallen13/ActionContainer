93c1805d97d69bbd545baa12a9ef79e3cc620bed
Evan Allen <eallen@xpirit.com>
2023-09-20 13:38:04 -0400

    change bash

--- SayHello.sh
+++ SayHello.sh
@@ -2,7 +2,14 @@
 
 repo_owner="evanallen13"
 repo_name="ActionContainer"
-response=$(gh issue list -R="$repo_owner/$repo_name" -s="open" -l=release)
+
+# response=$(gh issue list -R="$repo_owner/$repo_name" -l=release)
+response=$(gh issue list -R="$repo_owner/$repo_name" -l=release)
+
+if [[ -z "$response" ]]; then
+    echo "No response received. Check if there are release issues or if 'gh' is properly configured."
+    exit 1
+fi
 
 if echo "$response" | grep -q "no issues match your search"; then
     echo "IS_RELEASE=false"
