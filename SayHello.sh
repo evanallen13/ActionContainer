c6c8231476aa38ab709984b2ee663283aa8c0abe
Evan Allen <eallen@xpirit.com>
2023-09-20 11:06:19 -0400

    issue if statement

--- SayHello.sh
+++ SayHello.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+repo_owner="evanallen13"
+repo_name="ActionContainer"
 
-echo "this is in a bash script $1" 
+response=$(gh issue list -R="$repo_owner/$repo_name" -s="open" -l=bug)
+
+if echo "$response" | grep -q "no issues match your search"; then
+    echo "No ISSUES"
+else
+    echo $response
+fi
